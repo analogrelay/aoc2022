6d304a58b83911d8b6cf812e6886a626c5a0378d
Andrew Stanton-Nurse <git@analogrelay.net>
2022-12-04 15:51:31 +0000

    More templates for the rest of the days

--- src/day_04.adb
+++ src/day_04.adb
@@ -5,10 +5,8 @@ with AdventLib;                use AdventLib;
 with Ada.Strings.Maps;         use Ada.Strings.Maps;
 
 procedure Day_04 is
-   Invalid_Input : exception;
-
-   Dash_Set  : Character_Set := To_Set ('-');
-   Comma_Set : Character_Set := To_Set (',');
+   Dash_Set  : constant Character_Set := To_Set ('-');
+   Comma_Set : constant Character_Set := To_Set (',');
 
    type Assignment is record
       Min : Natural;
@@ -21,31 +19,38 @@ procedure Day_04 is
       Dash_Index := Index (Input, Dash_Set, 1);
       return
         (Min => Integer'Value (To_String (Head (Input, Dash_Index - 1))),
-         Max => Integer'Value (To_String (Tail (Input, Length(Input) - Dash_Index))));
+         Max =>
+           Integer'Value
+             (To_String (Tail (Input, Length (Input) - Dash_Index))));
    end Parse_Assignment;
 
-   function Left_Contains_Right(Left: Assignment; Right: Assignment) return Boolean is
+   function Left_Contains_Right
+     (Left : Assignment; Right : Assignment) return Boolean
+   is
    begin
-      return Left.Min >= Right.Min
-         and then Left.Min <= Right.Max
-         and then Left.Max >= Right.Min
-         and then Left.Max <= Right.Max;
+      return
+        Left.Min >= Right.Min and then Left.Min <= Right.Max
+        and then Left.Max >= Right.Min and then Left.Max <= Right.Max;
    end Left_Contains_Right;
 
-   function One_Contains_Other(First_Assignment :Assignment; Second_Assignment: Assignment) return Boolean is
+   function One_Contains_Other
+     (First_Assignment : Assignment; Second_Assignment : Assignment)
+      return Boolean
+   is
    begin
-      return Left_Contains_Right(First_Assignment, Second_Assignment) or else
-         Left_Contains_Right(Second_Assignment, First_Assignment);
+      return
+        Left_Contains_Right (First_Assignment, Second_Assignment)
+        or else Left_Contains_Right (Second_Assignment, First_Assignment);
    end One_Contains_Other;
 
-   function Overlaps(Left: Assignment; Right: Assignment) return Boolean is
+   function Overlaps (Left : Assignment; Right : Assignment) return Boolean is
    begin
       return not (Right.Min > Left.Max or else Left.Min > Right.Max);
    end Overlaps;
 
    procedure Part_1 (Input_Name : String) is
-      F : File_Type;
-      Count: Natural := 0;
+      F     : File_Type;
+      Count : Natural := 0;
    begin
       Open_Data_File (F, 4, Input_Name);
 
@@ -60,11 +65,12 @@ procedure Day_04 is
 
             Comma_Index := Index (Line, Comma_Set, 1);
 
-            First_Assignment  := Parse_Assignment (Head (Line, Comma_Index - 1));
+            First_Assignment  :=
+              Parse_Assignment (Head (Line, Comma_Index - 1));
             Second_Assignment :=
-              Parse_Assignment (Tail (Line, Length(Line) - Comma_Index));
+              Parse_Assignment (Tail (Line, Length (Line) - Comma_Index));
 
-            if One_Contains_Other(First_Assignment, Second_Assignment) then
+            if One_Contains_Other (First_Assignment, Second_Assignment) then
                Count := Count + 1;
             end if;
          end;
@@ -75,8 +81,8 @@ procedure Day_04 is
    end Part_1;
 
    procedure Part_2 (Input_Name : String) is
-      F : File_Type;
-      Count: Natural := 0;
+      F     : File_Type;
+      Count : Natural := 0;
    begin
       Open_Data_File (F, 4, Input_Name);
 
@@ -91,11 +97,12 @@ procedure Day_04 is
 
             Comma_Index := Index (Line, Comma_Set, 1);
 
-            First_Assignment  := Parse_Assignment (Head (Line, Comma_Index - 1));
+            First_Assignment  :=
+              Parse_Assignment (Head (Line, Comma_Index - 1));
             Second_Assignment :=
-              Parse_Assignment (Tail (Line, Length(Line) - Comma_Index));
+              Parse_Assignment (Tail (Line, Length (Line) - Comma_Index));
 
-            if Overlaps(First_Assignment, Second_Assignment) then
+            if Overlaps (First_Assignment, Second_Assignment) then
                Count := Count + 1;
             end if;
          end;
--- src/day_25.adb
+++ src/day_25.adb
@@ -1,7 +1,48 @@
-with Ada.Text_IO;
+with Ada.Strings.Unbounded;    use Ada.Strings.Unbounded;
+with Ada.Text_IO;              use Ada.Text_IO;
+with Ada.Text_IO.Unbounded_IO; use Ada.Text_IO.Unbounded_IO;
+with AdventLib;                use AdventLib;
 
 procedure Day_25 is
+   procedure Part_1 (Input_Name : String) is
+      F : File_Type;
+      Result: Natural;
+   begin
+      Open_Data_File (F, 25, Input_Name);
+
+      while not End_Of_File (F) loop
+         declare
+            Line              : Unbounded_String;
+         begin
+            Line := Get_Line (F);
+         end;
+      end loop;
+
+      Put_Line ("Part 1 " & Input_Name & " Result: " & Result'Image);
+      Close (F);
+   end Part_1;
+
+   procedure Part_2 (Input_Name : String) is
+      F : File_Type;
+      Result: Natural;
+   begin
+      Open_Data_File (F, 25, Input_Name);
+
+      while not End_Of_File (F) loop
+         declare
+            Line              : Unbounded_String;
+         begin
+            Line := Get_Line (F);
+         end;
+      end loop;
+
+      Put_Line ("Part 2 " & Input_Name & " Result: " & Result'Image);
+      Close (F);
+   end Part_2;
 begin
-   Ada.Text_IO.Put_Line ("Advent of Code Day 25");
-   Ada.Text_IO.Put_Line ("Not yet implemented");
+   Put_Line ("Advent of Code Day 25");
+   --  Part_1 ("test");
+   --  Part_1 ("input");
+   --  Part_2 ("test");
+   --  Part_2 ("input");
 end Day_25;
